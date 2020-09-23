b5221a9a12fbe6bb66f5b2fe0df2203e74d642aa
Olexandr <alexandr90h@gmail.com>
2020-09-23 23:06:25 +0300

    modal close fix

--- js/gallery-list.js
+++ js/gallery-list.js
@@ -1,8 +1,7 @@
 import galleryListObj from "./gallery-items.js";
-console.log(galleryListObj);
 const galleryListElem = document.querySelector(".js-gallery");
 const modalElem = document.querySelector(".js-lightbox");
-const btnCloseElem = document.querySelector(".lightbox__button");
+const originalImagesElem = document.querySelector(".lightbox__image");
 
 function createListGallery(gall) {
   return gall
@@ -23,35 +22,23 @@ function getUrl(evt) {
   if (evt.target.nodeName !== "IMG") {
     return;
   }
-  console.log(evt.target.dataset.source);
+  evt.preventDefault();
+  originalImagesElem.setAttribute("src", evt.target.dataset.source);
   modalElem.classList.add("is-open");
 }
+function closeModal(evt) {
+  if (
+    evt.target.dataset.action === "close-lightbox" ||
+    evt.target.classList.contains("lightbox__overlay")
+  ) {
+    evt.currentTarget.classList.remove("is-open");
+    originalImagesElem.removeAttribute("src");
+  }
+}
+
 galleryListElem.insertAdjacentHTML(
   "afterbegin",
   createListGallery(galleryListObj)
 );
-function closeModal() {
-  modalElem.classList.remove("is-open");
-}
-
 galleryListElem.addEventListener("click", getUrl);
-btnCloseElem.addEventListener("click", closeModal);
-// galleryListObj.forEach(({ preview, original, description }) => {
-//   const itemOfListGalleryElem = document.createElement("li");
-//   const linkOfListGalleryElem = document.createElement("a");
-//   const imagesOfListGalleryElem = document.createElement("img");
-
-//   itemOfListGalleryElem.classList.add("gallery__item");
-//   linkOfListGalleryElem.classList.add("gallery__link");
-//   linkOfListGalleryElem.setAttribute("href", original);
-//   imagesOfListGalleryElem.classList.add("gallery__image");
-//   imagesOfListGalleryElem.setAttribute("src", preview);
-//   imagesOfListGalleryElem.setAttribute("data-source",original);
-//   imagesOfListGalleryElem.setAttribute("alt", description);
-
-//   galleryListElem.append(itemOfListGalleryElem);
-//   itemOfListGalleryElem.append(linkOfListGalleryElem);
-//   linkOfListGalleryElem.append(imagesOfListGalleryElem);
-// });
-
-//комент в css і в href-ссилки додати змінну
+modalElem.addEventListener("click", closeModal);
