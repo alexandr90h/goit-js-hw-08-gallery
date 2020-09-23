f06fd4334e0a1290b0574be4aa25acfd14dce3e8
Olexandr <63474923+alexandr90h@users.noreply.github.com>
2020-09-23 17:39:49 +0300

    modal fix

--- js/gallery-list.js
+++ js/gallery-list.js
@@ -1,13 +1,17 @@
 import galleryListObj from "./gallery-items.js";
 console.log(galleryListObj);
 const galleryListElem = document.querySelector(".js-gallery");
+const modalElem = document.querySelector(".js-lightbox");
+const btnCloseElem = document.querySelector(".lightbox__button");
 
 function createListGallery(gall) {
   return gall
     .map(({ preview, original, description }) => {
       return `<li class="gallery__item">
-    <a class="gallery__link"
-    href="${original}">
+      <a
+    class="gallery__link"
+    href="${original}"
+  >
     <img class="gallery__image"
       src="${preview}"
       data-source="${original}"
@@ -16,14 +20,22 @@ function createListGallery(gall) {
     .join("");
 }
 function getUrl(evt) {
+  if (evt.target.nodeName !== "IMG") {
+    return;
+  }
   console.log(evt.target.dataset.source);
+  modalElem.classList.add("is-open");
 }
 galleryListElem.insertAdjacentHTML(
   "afterbegin",
   createListGallery(galleryListObj)
 );
+function closeModal() {
+  modalElem.classList.remove("is-open");
+}
 
 galleryListElem.addEventListener("click", getUrl);
+btnCloseElem.addEventListener("click", closeModal);
 // galleryListObj.forEach(({ preview, original, description }) => {
 //   const itemOfListGalleryElem = document.createElement("li");
 //   const linkOfListGalleryElem = document.createElement("a");
@@ -41,3 +53,5 @@ galleryListElem.addEventListener("click", getUrl);
 //   itemOfListGalleryElem.append(linkOfListGalleryElem);
 //   linkOfListGalleryElem.append(imagesOfListGalleryElem);
 // });
+
+//комент в css і в href-ссилки додати змінну
